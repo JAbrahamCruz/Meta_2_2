c65197ab322a8605bd340a4fd58f8985469c9c0e
Sr.Drunkenstein <abraham.cruz.dev@gmail.com>
2025-09-29 12:06:11 -0700

    Se agrega la meta 2.2

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+//
+</script>
+
+<style scoped>
+/* * {
+  border: 1px solid white;
+} */
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container class="fill-height d-flex flex-column justify-center align-center text-center">
+    <h1 class="text-h2 font-weight-bold mb-6">Bienvenido a mi Landing 🚀</h1>
+    <p class="text-subtitle-1 mb-8">Un texto breve que explique de qué va tu proyecto.</p>
+    <v-btn @click="theme.toggle()" text="Toggle Light / Dark"></v-btn>
+  </v-container>
+</template>
+
+<script setup>
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+</script>
